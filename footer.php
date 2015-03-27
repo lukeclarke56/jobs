ab31a1164c402c0c13a86071c5ea90a6de1fb0d9
lukeclarke56 <lukeclarke56@hotmail.co.uk>
2015-03-27 15:29:47 +0000

    footerdrafft1.php
    This was the first footer I created for the website

---
+++ footer.php
@@ -0,0 +1,41 @@
+    <div id="footer">
+        <div class="bottom-container footer">
+            <div class="container">
+                <div class="two columns alpha">
+                    <h4>Home</h4>
+                    <ul>
+                    <li><a href="#" class=" blank-link">Sign in</a></li>  
+                    <li><a href="#" class=" blank-link">jobs search</a></li>
+                    </ul>
+                </div>
+                <div class="two columns">
+                    <h4>About Us</h4>
+                        <ul>
+                        <li><a href="/final/contact/contact.html" class=" ">Contact Us</a></li>
+                        <li><a href="#" class=" blank-link">Help</a></li>
+                        </ul>
+                </div>
+                <div class="two columns">
+                    <h4>Contact</h4>
+                        <ul>
+                        <li><a href="/final/contact/contact.html" class=" ">Contact us</a></li>
+                        <li><a href="#" class=" ">Privacy Policy</a></li>
+                        <li><a href="#" class=" ">Terms of Use</a></li>
+                        </ul>
+                </div>
+                <div class="six columns">
+                    <p class="tright">
+                        <a href="#" class="iconned twitter blank-link" title="Twitter"></a>
+                        <a href="#"class="iconned facebook blank-link" title="Facebook"></a>        
+                    </p>
+                    <p class="tright">&copy; jobs4students 2015.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<script src="js/toucheffects.js"></script>
+
+    </body>
+</html>
